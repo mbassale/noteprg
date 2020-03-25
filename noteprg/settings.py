541acb2821632e370ff14c010f26b27495af8ef8
Marco Bassaletti <bassaletti@gmail.com>
2020-03-25 02:32:42 -0300

    Added lorem ipsum generator application.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- noteprg/settings.py
+++ noteprg/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'lorem',
+    'notes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- noteprg/urls.py
+++ noteprg/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('lorem/', include('lorem.urls')),
     path('notes/', include('notes.urls')),
     path('admin/', admin.site.urls),
 ]
