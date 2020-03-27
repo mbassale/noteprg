844c4595b395c156fd7b1f72818d828dc0649c13
Marco Bassaletti <bassaletti@gmail.com>
2020-03-27 01:58:58 -0300

    Notes: base template layout and root url redirection.
    Signed-off-by: Marco Bassaletti <bassaletti@gmail.com>

--- noteprg/urls.py
+++ noteprg/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
+    path('', lambda r: HttpResponseRedirect('notes/')),
     path('lorem/', include('lorem.urls')),
     path('notes/', include('notes.urls')),
     path('admin/', admin.site.urls),
